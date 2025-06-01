dc1af87f6e408cf45214f50204d1c28f0c7de90b
Moses <awedamoses@gmail.com>
2025-06-01 17:56:32 +0100

    Remove key from code and use env

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider  } from '@react-oauth/google';
 
-const CLIENT_ID = "235778258186-5uv9grf9jq6st83cks5sej6ii9t0nvge.apps.googleusercontent.com"
-
+// const CLIENT_ID = "GOOGLE_CLIENT_ID"; 
+const CLIENT_ID =  process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
